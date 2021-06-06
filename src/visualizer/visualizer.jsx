adde34685be8e346196112b39851d1cdda1b62ca
Patrick <phwang4@wisc.edu>
2021-06-06 12:53:06 -0500

    add canvas

--- src/visualizer/visualizer.jsx
+++ src/visualizer/visualizer.jsx
@@ -1,11 +1,14 @@
 import { Component } from "react";
+import "./visualizer.css"
 
 export default class Visualizer extends Component {
   render() {
     return (
-      <div>
-        <h1>From visualizer.jsx</h1>
-      </div>
+        <div>
+          <h1>Visualizer</h1>
+          <canvas id="myCanvas"></canvas>
+        </div>
     )
   }
 }
+
