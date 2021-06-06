50ac1eaa33f48c6a6aa41d5605f723321653033e
Patrick <phwang4@wisc.edu>
2021-06-06 09:47:03 -0500

    setup react

---
+++ visualizer/src/visualizer/visualizer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Visualizer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>From visualizer.jsx</h1>
+      </div>
+    )
+  }
+}
